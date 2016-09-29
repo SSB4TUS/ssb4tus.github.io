2f58590e762b9c0ff68beef1421bfe840ec32c28
yuya sato <mrfool38@gmail.com>
2016-09-29 23:49:32 +0900

    update kokucheese_export.js

--- assets/js/kokucheese_export.js
+++ assets/js/kokucheese_export.js
@@ -7,7 +7,12 @@ var targets = [
   '[data-role="content"] [data-role="listview"]:nth-child(6)',
 ];
 var $removeTarget = $(targets.join());
+
+var html = '<a href="https://ssl.kokucheese.com/event/entry/429832/"><input type="button" class="register-button" value="今すぐ理科ブラ6に参加登録する！"></a>';
+
 function jQueryMain() {
   $removeTarget.remove();
+  $('.register-target').append(html);
 }
+
 $(jQueryMain);
